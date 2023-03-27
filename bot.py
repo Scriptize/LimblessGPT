fa1dbe08e2f38063851e988ac08d00f9fe4542a6
Scriptize <dblaylock8915@gmail.com>
2023-03-27 17:10:45 -0400

    Updates
    *Implemented context resizing
    *Theoretical 24/7  working uptime if deployed

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import hikari
 import lightbulb
 import openai
 import datetime
-from gpt_api import chat_call
+from gpt_api import chat_call,davinci_call
 
 bot = lightbulb.BotApp(token='YOUR TOKEN HERE', help_slash_command=True,intents=hikari.Intents.GUILD_MESSAGES,default_enabled_guilds=())
 
@@ -15,52 +15,120 @@ context =[
         ]
     
 @bot.command()
-@lightbulb.command("help", "getting started")
+@lightbulb.command("info", "getting started")
 async def help(message):
     await message.respond("Hello, I am Limbless's AI model.\nStart your prompt with 'Limbless,' to begin.")
 
 @bot.listen(hikari.GuildMessageCreateEvent)
 async def get_reply(event):
         global context
-        #content_value = data['choices'][0]['message']['content']
-        username = event.message.author.username
+        
+        username = event.message.author.username # user that sent message
         
 
-        if event.message.content.startswith('Limbless,'):
-            context.append({"role":"system", "content": f"The following message is by {username}: {event.message.content[10:]}", "guild_id":event.guild_id})
+        if event.message.content.startswith('Limbless,'): 
 
-            filtered_context = [message for message in context[6:] if message['guild_id'] == event.guild_id]
-            guild_context = [{'role': message['role'], 'content': message['content']} for message in filtered_context]
+            if len(event.message.content[10:]) >= 2000:
+                await event.message.respond("Your request is too long, please lower it to at most 2000 characters", reply=True,mentions_reply=True)
+            
+            else: 
+                
+                context.append({"role":"system", "content": f"The following message is by {username}: {event.message.content[10:]}", "guild_id":event.guild_id}) 
+                print({"The context length is:":len(context)})
+                
+                filtered_context = [message for message in context[6:] if message['guild_id'] == event.guild_id]
+                
+                guild_context = [{'role': message['role'], 'content': message['content']} for message in filtered_context]
 
-            try:
-                content_req = chat_call(guild_context)
-            except openai.error.InvalidRequestError:
-                print(len(guild_context))
-                await event.message.respond("Give me a moment, I'm resizing my context capacity...", reply=True,mentions_reply=True)
-                guild_context = context[:6] + guild_context[int(len(context) * 3/4):]
-                content_req = chat_call(guild_context)
-            response = content_req["choices"][0]["message"]["content"]
-            context.append({"role":"assistant", "content": response,"guild_id":event.guild_id})
-            await event.message.respond(event.message.author.mention + ", " + response,reply=True,mentions_reply=True)
-            print(context)
-        elif username != "LimblessGPT":
+                try: 
+                    content_req = chat_call(context[:6] + guild_context)
+
+                
+                except openai.error.InvalidRequestError:
+                    print({"Rezing at guild context length":len(guild_context)})
+                    await event.message.respond("Give me a moment, I'm resizing my context capacity...", reply=True,mentions_reply=True)
+                    =
+                    guild_msg_count = sum("guild_id" in d and d["guild_id"] == event.guild_id for d in context)
+                    
+                    new_context = context[:6]
+                    count = 0
+                    
+                    for i in context[6:]:
+                        
+                        if i["guild_id"] != event.guild_id:
+                            new_context.append(i)
+                        
+                        if i["guild_id"] == event.guild_id:
+                            count +=1
+                        
+                        if i["guild_id"] == event.guild_id and (count > (3 * (guild_msg_count//4))):
+                            new_context.append(i)
+                    
+                    filtered_context = [message for message in new_context[6:] if message['guild_id'] == event.guild_id]
+                    
+                    guild_context = [{'role': message['role'], 'content': message['content']} for message in filtered_context]
+                    try:
+                        content_req = chat_call(new_context[6:] + guild_context)
+                    except hikari.errors.BadRequestError: 
+                        await event.message.respond("`SYSTEM` The response to this request is too long.",reply=True,mentions_reply=True)
+                    
+                    context = new_context
+                    print({"The context length is:":len(context)})
+                
+                except hikari.errors.BadRequestError:
+                        await event.message.respond("`SYSTEM` The response to this request is too long.",reply=True,mentions_reply=True)
+                    
+                response = content_req["choices"][0]["message"]["content"]
+                context.append({"role":"assistant", "content": response,"guild_id":event.guild_id})
+                await event.message.respond(event.message.author.mention + ", " + response,reply=True,mentions_reply=True)
+                print({"The context length is:":len(context)})
+
+        elif username != "Limbless_GPT":
             context.append({"role":"system", "content": f"The following message is by {username}: {event.message.content}", "guild_id":event.guild_id})
-            print(context)
-        if len(context)>= 120:
-            await event.message.respond("Please wait, I'm reszing platform context for efficency", reply=True,mentions_reply=True)
-            context = context[:6] + context[int(len(context) * 3/4):]
+            print({"The context length is:":len(context)})
+
+    
+        
+@bot.command()
+@lightbulb.command("davinci", "Starts davinci model")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def start_convo(message):
+
+    
+    await message.respond("I am Davinci, a completion model.\nStart your prompt with 'Davinci,' to begin.\n(WARNING: THIS MODEL IS NOT WELL TRAINED AGAINST OFFENSIVE CONTENT)")
+
+    @bot.listen(hikari.GuildMessageCreateEvent)
+    async def get_reply(event):
+            
+            
+            #content_value = data['choices'][0]['message']['content']
+            #username = event.message.author.username
+            if event.message.content.startswith('Davinci,'):
+                if event.message.content == "Davinci, GLOBAL SHUTDOWN":
+                        bot.unsubscribe(hikari.GuildMessageCreateEvent, get_reply)
+                        proceed = False
+                if proceed:
+                    #context.append({"role":"system", "content": f"The following message is by {username}: {event.message.content[10:]}"})
+                    content_req = davinci_call(event.message.content[10:])
+                    response = content_req["choices"][0]["text"]
+                    #context.append({"role":"assistant", "content": response})
+                    await event.message.respond(event.message.author.mention + ", " + response,reply=True,mentions_reply=True)
+                    print(response)
+
+
+               
 
 @bot.command()
-@lightbulb.option('channelid','which channel?',type=int)
-@lightbulb.option('timelapse','from how many hours until now?',type=int)
-@lightbulb.option('userid','which user',type=int)
+@lightbulb.option('channelid','which channel?',type=str)
+@lightbulb.option('timelapse','from how many hours until now?',type=str)
+@lightbulb.option('userid','which user',type=str)
 @lightbulb.command("delete", "deletes history")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def delete_history(ctx):
     # bulk delete only allows deleting messages younger than 14 days
-    bulk_delete_limit = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=3)
-    async for message in bot.rest.fetch_messages(ctx.options.channelid):
-        if message.created_at > bulk_delete_limit and (message.author.id == ctx.options.userid):
-            await bot.rest.delete_message(ctx.options.channelid, message.id) 
-            
+    bulk_delete_limit = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=int(ctx.options.timelapse))
+    async for message in bot.rest.fetch_messages(int(ctx.options.channelid)):
+        if message.created_at > bulk_delete_limit and (message.author.id == int(ctx.options.userid)):
+            await bot.rest.delete_message(int(ctx.options.channelid), message.id)
+
 bot.run()
